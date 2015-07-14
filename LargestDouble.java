c6a461d8b389cb5ef1b34031c2db3c1ba6bdafb8
Alex Dzyoba <avd@reduct.ru>
2015-07-14 13:26:33 +0300

    LargestDouble - DOUBLE INFINITY!

---
+++ LargestDouble.java
@@ -0,0 +1,10 @@
+class LargestDouble {
+	public static void main (String [] args)
+	{
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.POSITIVE_INFINITY);
+		System.out.println(Double.NEGATIVE_INFINITY);
+		System.out.println(Math.nextUp(Double.MAX_VALUE));
+		System.out.println(Math.nextUp(Double.POSITIVE_INFINITY));
+	}
+}
