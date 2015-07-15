637349f65d27f353ee9ba7f81d047f2ff738c986
Alex Dzyoba <avd@reduct.ru>
2015-07-15 18:56:50 +0300

    MyQueue.java - inner classes, static context, crazy syntax.

---
+++ MyQueue.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class MyQueue {
+	private List<Node> queue;
+
+	MyQueue()
+	{
+		queue = new LinkedList<Node>();
+	}
+
+	public void add(Node n)
+	{
+		queue.add(n);
+	}
+
+	public void addString(String s)
+	{
+		Node n = new Node(s);
+		add(n);
+	}
+
+	public Node remove()
+	{
+		Node n = queue.get(0);
+		queue.remove(0);
+		return n;
+	}
+
+	/**
+	 * Inner class representing queue node.
+	 *
+	 * It's not static to show how crazy is syntax.
+	 */
+	public class Node {
+		private String string;
+		
+		Node(String s)
+		{
+			string = s;
+		}
+
+		String value()
+		{
+			return string;
+		}
+	}
+
+	public static void main (String [] args)
+	{
+		MyQueue q = new MyQueue();
+
+		/*
+		 * XXX:
+		 * Using q.new is necessary because main is static function.
+		 * Compare it with q.addString. addString is not static so we 
+		 * can just use new without specifying context for it.
+		 */
+		q.add(q.new Node("1"));
+		q.addString("2");
+
+		System.out.println(q.remove().value());
+		q.add(q.new Node("3"));
+		q.add(q.new Node("4"));
+		System.out.println(q.remove().value());
+		q.add(q.new Node("5"));
+		System.out.println(q.remove().value());
+		System.out.println(q.remove().value());
+	}
+}
